3821c6b0c70c0189434d0c1c4cdaa15dbaed4938
ragul <ragul0028@gmail.com>
2024-08-17 21:55:34 +0530

    first commit

---
+++ auth.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, render_template, redirect, url_for,request
+from .models import User
+from . import db
+from flask_login import login_user, logout_user, login_required
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+
+
+    return render_template('login.html')
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = User.query.filter_by(email=email).first()
+    
+    if not user or user.password != password:
+        return redirect(url_for('auth.signup'))  # Redirect back to login if authentication fails
+
+    login_user(user)
+    return redirect(url_for('main.profile'))
+
+
+@auth.route('/signup')
+def signup():
+  
+    
+    return render_template('signup.html')
+
+@auth.route('/signup_post', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+    
+
+   # print(email, name, password)
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        print("user already exist")
+    else:
+        new_user = User(email=email,password=password,name=name)
+        db.session.add(new_user)
+        db.session.commit()
+    return redirect(url_for('auth.login'))
