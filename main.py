0230ea09aa00955deb42e4d3ab2a5e0044799b03
ragul <ragul0028@gmail.com>
2024-09-01 11:55:08 +0530

    commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request,flash,redirect,url_for
+
 from flask_login import login_required, current_user
+
+from .models import Patient,db
+from .models import User
 
 main = Blueprint('main', __name__)
 
@@ -11,4 +15,64 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html')
+@main.route('/new')
+@login_required
+def new_patient():
+    return render_template('create_patient.html')
+@main.route('/new', methods=['POST'])
+@login_required
+def new_patient_post():
+    name = request.form['name']
+    age = request.form['age']
+    sex = request.form['sex']
+    cp = request.form['cp']
+    trestbps = request.form['trestbps']
+    chol = request.form['chol']
+    fbs = request.form['fbs']
+    restecg = request.form['restecg']
+    thalach = request.form['thalach']
+    exang = request.form['exang']
+    oldpeak = request.form['oldpeak']
+    slope = request.form['slope']
+    ca = request.form['ca']
+    thal = request.form['thal']
+    target = request.form['target']
 
+    new_entry = Patient(
+        name=name,
+        age=age,
+        sex=sex,
+        cp=cp,
+        trestbps=trestbps,
+        chol=chol,
+        fbs=fbs,
+        restecg=restecg,
+        thalach=thalach,
+        exang=exang,
+        oldpeak=oldpeak,
+        slope=slope,
+        ca=ca,
+        thal=thal,
+        target=target,
+        user_id = current_user.id
+    )
+
+    db.session.add(new_entry)
+    db.session.commit()
+
+    flash('Your workout has been added!')
+
+    return redirect(url_for('main.user_patients'))
+
+
+@main.route('/all')
+@login_required
+def user_patients():
+    # Fetch the user based on current_user's email
+    user = User.query.filter_by(email=current_user.email).first_or_404()
+
+    # Get all patients related to the user
+    patients = user.patients
+
+    # Render the template with the patient's data
+    return render_template('all_patients.html', patients=patients, user=user)
--- models.py
+++ models.py
@@ -1,9 +1,28 @@
 from . import db
 from flask_login import UserMixin
 
-
-class User(db.Model,UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
+    patients = db.relationship('Patient', backref='author', lazy=True)
+
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    sex = db.Column(db.Integer, nullable=False)
+    cp = db.Column(db.Integer, nullable=False)
+    trestbps = db.Column(db.Integer, nullable=False)
+    chol = db.Column(db.Integer, nullable=False)
+    fbs = db.Column(db.Integer, nullable=False)
+    restecg = db.Column(db.Integer, nullable=False)
+    thalach = db.Column(db.Integer, nullable=False)
+    exang = db.Column(db.Integer, nullable=False)
+    oldpeak = db.Column(db.Float, nullable=False)
+    slope = db.Column(db.Integer, nullable=False)
+    ca = db.Column(db.Integer, nullable=False)
+    thal = db.Column(db.Integer, nullable=False)
+    target = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Foreign key to User
